80d06a8f535c7a907bc7acc04f5706685f1a9f0b
yuto-matsuda <yutochin.dev@gmail.com>
2025-07-19 00:25:43 +0900

    image/jpeg and metadata in header

--- routes/post.py
+++ routes/post.py
@@ -8,10 +8,10 @@ post_bp = Blueprint('post', __name__)
 
 @post_bp.route('/post', methods=['POST'])
 def post():
-    cam_id        = int(request.form.get('cid'))
-    cam_timestamp = request.form.get('ts')
-    sensor_data   = json.loads(request.form.get('sensor'))
-    img           = request.files.get('img')
+    img           = request.data
+    cam_id        = int(request.headers.get('cid'))
+    cam_timestamp = request.headers.get('ts')
+    sensor_data   = json.loads(request.headers.get('sensor'))
     gps_timestamp = sensor_data.get('gpsTs')
     lat           = sensor_data.get('lat')
     lng           = sensor_data.get('lng')
@@ -22,6 +22,10 @@ def post():
         acc['ax'] = float(acc['ax'])
         acc['ay'] = float(acc['ay'])
         acc['az'] = float(acc['az'])
+
+    # print(cam_id)
+    # print(cam_timestamp)
+    # print(sensor_data)
 
     if not img:
         return 'Invalid request', 400
@@ -46,3 +50,48 @@ def post():
         return 'Success', 200
     except Exception as e:
         return 'DB error', 500
+
+# multipart/form-data用
+# @post_bp.route('/post', methods=['POST'])
+# def post():
+#     data = request.get_data()
+#     print("Actual received body length:", len(data))
+
+#     cam_id        = int(request.form.get('cid'))
+#     cam_timestamp = request.form.get('ts')
+#     sensor_data   = json.loads(request.form.get('sensor'))
+#     img           = request.files.get('img')
+#     gps_timestamp = sensor_data.get('gpsTs')
+#     lat           = sensor_data.get('lat')
+#     lng           = sensor_data.get('lng')
+#     spd           = sensor_data.get('spd')
+#     accs          = sensor_data.get('accs')
+
+#     for acc in accs:
+#         acc['ax'] = float(acc['ax'])
+#         acc['ay'] = float(acc['ay'])
+#         acc['az'] = float(acc['az'])
+
+#     if not img:
+#         return 'Invalid request', 400
+    
+#     fname = f'{cam_id}_{cam_timestamp}'.replace(':', '-')
+#     result = cloudinary.uploader.upload(img, public_id=fname)
+#     url = result['secure_url']
+    
+#     data = {
+#         'camera_id':        cam_id,
+#         'camera_timestamp': cam_timestamp,
+#         'gps_timestamp':    gps_timestamp,
+#         'latitude':         lat,
+#         'longitude':        lng,
+#         'speed':            spd,
+#         'accelerations':    accs,
+#         'image_url':        url,
+#     }
+
+#     try:
+#         supabase.table('pavement').insert(data).execute()
+#         return 'Success', 200
+#     except Exception as e:
+#         return 'DB error', 500
